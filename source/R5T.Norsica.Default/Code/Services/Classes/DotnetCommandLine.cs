5339207cc4efb5694a25297868eb6713e279ff5c
MinexAutomation <minex.automation@gmail.com>
2019-12-14 23:52:48 -0800

    Added DotnetCommandLine namespace forward of R5T.Volos.CommandLine. Implemented DotnetOperator.AddNewSolutionFile() method.

---
+++ source/R5T.Norsica.Default/Code/Services/Classes/DotnetCommandLine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using R5T.Heraklion;
+using R5T.Volos;
+
+using R5T.Norsica.Commands;
+
+
+namespace R5T.Norsica.Default
+{
+    public static class DotnetCommandLine
+    {
+        public static ICommandBuilderContext<DotnetContext> New()
+        {
+            var dotnetContext = CommandLine.New<DotnetContext>();
+            return dotnetContext;
+        }
+    }
+}
---
+++ source/R5T.Norsica.Default/Code/Services/Definitions/DotnetOperator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using R5T.Caledonia;
+using R5T.Heraklion;
+
+using R5T.Norsica.Commands;
+using R5T.Norsica.Configuration;
+
+
+namespace R5T.Norsica.Default
+{
+    public class DotnetOperator : IDotnetOperator
+    {
+        private ICommandLineInvocationOperator CommandLineInvocationOperator { get; }
+        private IDotnetExecutableFilePathProvider DotnetExecutableFilePathProvider { get; }
+
+
+        public DotnetOperator(ICommandLineInvocationOperator commandLineInvocationOperator, IDotnetExecutableFilePathProvider dotnetExecutableFilePathProvider)
+        {
+            this.CommandLineInvocationOperator = commandLineInvocationOperator;
+            this.DotnetExecutableFilePathProvider = dotnetExecutableFilePathProvider;
+        }
+
+        private void Execute(string arguments)
+        {
+            var gitExecutableFilePath = this.DotnetExecutableFilePathProvider.GetDotnetExecutableFilePath();
+
+            var invocation = CommandLineInvocation.New(gitExecutableFilePath, arguments);
+
+            var result = this.CommandLineInvocationOperator.Run(invocation);
+
+            if (result.ExitCode != 0)
+            {
+                throw new Exception($"Execution failed. Error:\n{result.GetErrorText()}\nOutput:\n{result.GetOutputText()}\nArguments:\n{arguments}");
+            }
+            else
+            {
+                Console.WriteLine(result.GetOutputText());
+                Console.WriteLine(result.GetErrorText());
+            }
+        }
+
+        public void CreateNewSolutionFile(string solutionDirectoryPath, string solutionName)
+        {
+            var command = DotnetCommandLine.New().New()
+                .Sln()
+                .SetOutputDirectoryPath(solutionDirectoryPath)
+                .SetName(solutionName)
+                .BuildCommand();
+
+            this.Execute(command);
+        }
+    }
+}
